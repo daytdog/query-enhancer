68fa11a86d9bfe57374fffb18536d5c61bfcb576
Craig Ignatowski <cignatow@asu.edu>
2018-11-11 05:21:01 -0700

    Updated MongoDB connection to the engine
    - Also removed several unnecessary files/directories

--- jsonTools.py
+++ jsonTools.py
@@ -3,41 +3,22 @@
 jsonTools.py
 """
 
-def recFetcher(obj):
-    """
-    Recursive function which can print the values,
-    only if they are neither a list nor a dictionary
-    """
-    if isinstance(obj, list):       #if the object is a list
-        for item in obj:            #go into the list
-            for value in recFetcher(item):      #run recFetcher recursively
-                yield value         #yields the value given by the recFetcher recursion
-    elif isinstance(obj, dict):     #if the object is a dictionary
-        for item in obj:            #for the items in the dictionary
-            for value in recFetcher(obj[item]): #run the items through recFetcher recursively
-                yield value         #yields the value given by the recFetcher recursion
-    else:
-        yield obj                   #else, just yield the final object
-
-
-def parseMongoRecord(mdb_record):
+def parseMongoRecord(mdb_record,dict_field_name):
     """
     Expects a record containing an '_id' key, and other
     important non-selection field keys such as summary,
     details, etc.
     """
     record_id = mdb_record.pop('_id', None)
-    record_str = " ".join(list(recFetcher(mdb_record)))
-    return (record_id, record_str)
+    record_dict = mdb_record[dict_field_name]
+    return (record_id, record_dict)
 
 
-def getText(mdb_cursor): #timeit() avg time for 116 records: 5.55 ms
+def getText(mdb_cursor, dict_field_name):
     """
     Creates a list of (record_id,record_str) tuples from a given MongoDB query
     """
     corpus = []
     for record in mdb_cursor:
-        corpus.append(parseMongoRecord(record))
-    #Note: Dictionaries are unordered, so the values from dictionary may
-    #not be in the same position, as in the literal passed
+        corpus.append(parseMongoRecord(record, dict_field_name))
     return corpus
